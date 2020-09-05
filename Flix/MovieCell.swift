0ce9fee53bd4a9df3507ac082e803e2497495cd3
Xochitl Medina <medinaxochitl98@gmail.com>
2020-09-05 12:42:05 -0700

    Added all the important features for the basic app, without the bonus for now.

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Xochitl Medina on 9/5/20.
+//  Copyright © 2020 Xochitl Medina. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
